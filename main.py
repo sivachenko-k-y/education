f33a8d3899aab96c67772c7a2d988a4baff9c6e6
sivachenko-k-y <sivachenko.k.y@gmail.com>
2022-07-13 22:24:36 +0300

    create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+
+
+class MyApp(App):                   #имя класса заканчиваться на App
+    pass
+
+
+if __name__ == "__main__":
+    MyApp().run()                   #создаем экземпляр и вызываем метод

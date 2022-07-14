a4f113ed447bf6a5fc54de260f230b7c06442bfd
sivachenko-k-y <sivachenko.k.y@gmail.com>
2022-07-14 11:36:01 +0300

    button start

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from kivy.app import App
+from kivy.app import App            # класс "окно\приложение"
+from kivy.uix.button import Button  # класс "кнопка"
 
 
-class MyApp(App):                   #имя класса заканчиваться на App
-    pass
+class MyApp(App):# имя класса - имя загаловка окна пр., должно заканчиваться на App
+
+    def build(self):
+        btn1 = Button(text='Hello world', font_size=14)
+
+        def callback(instance):
+            print('The button <%s> is being pressed' % instance.text)
+
+        btn1.bind(on_press=callback)
+
+        return btn1
 
 
-if __name__ == "__main__":
-    MyApp().run()                   #создаем экземпляр и вызываем метод
+
+
+
+if __name__ == "__main__":          # если текущий модуль это  __main__,то
+    MyApp().run()                   # создаем экземпляр и вызываем метод
